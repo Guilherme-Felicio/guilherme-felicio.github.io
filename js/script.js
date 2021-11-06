163dbbcee9f970904f6b5334e927175950dd77a3
Felicio <guifelicio7@gmail.com>
2021-11-06 17:44:28 -0300

    Melhorei função de animar graficos

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ const setaDireita = document.querySelectorAll(".setaDir");
 const projetos = document.querySelectorAll(".projeto");
 let projetoAtivo = 0;
 
+//habilidades
+const habilidades = document.querySelector("#habilidades")
+
 // grafico
 const graficos = document.querySelectorAll(".grafico");
 const small = window.matchMedia("(min-width: 576px)").matches
@@ -50,8 +53,10 @@ function movimentarBarco(){
 }
 
 function animarGrafico(graficos){
-    if(xl){
-        if(window.pageYOffset > 650){
+    const alturaTela = window.innerHeight;
+    const distanciaTopo = habilidades.getBoundingClientRect().top //pega a distancia do elemento para o topo da página
+    if(distanciaTopo <= alturaTela * 0.4){
+        
             graficos[0].style.paddingRight = "60%";
             graficos[0].style.transition = "1.5s";
             graficos[1].style.paddingRight = "55%";
@@ -64,74 +69,8 @@ function animarGrafico(graficos){
             graficos[4].style.transition = "1.9s";
             graficos[5].style.paddingRight = "22%";
             graficos[5].style.transition = "2s";
-        }
-    } 
-
-    else if(lg){
-        if(window.pageYOffset > 670){
-            graficos[0].style.paddingRight = "60%";
-            graficos[0].style.transition = "1.5s";
-            graficos[1].style.paddingRight = "55%";
-            graficos[1].style.transition = "1.6s";
-            graficos[2].style.paddingRight = "47%";
-            graficos[2].style.transition = "1.7s";
-            graficos[3].style.paddingRight = "24%";
-            graficos[3].style.transition = "1.8s";
-            graficos[4].style.paddingRight = "22%";
-            graficos[4].style.transition = "1.9s";
-            graficos[5].style.paddingRight = "22%";
-            graficos[5].style.transition = "2s";
-        }
-    } 
-
-    else if(md){
-        if(window.pageYOffset > 800){
-            graficos[0].style.paddingRight = "60%";
-            graficos[0].style.transition = "1.5s";
-            graficos[1].style.paddingRight = "55%";
-            graficos[1].style.transition = "1.6s";
-            graficos[2].style.paddingRight = "47%";
-            graficos[2].style.transition = "1.7s";
-            graficos[3].style.paddingRight = "24%";
-            graficos[3].style.transition = "1.8s";
-            graficos[4].style.paddingRight = "22%";
-            graficos[4].style.transition = "1.9s";
-            graficos[5].style.paddingRight = "22%";
-            graficos[5].style.transition = "2s";
-        }
-    } 
-
-    else if(small){
-        if(window.pageYOffset > 930){
-            graficos[0].style.paddingRight = "60%";
-            graficos[0].style.transition = "1.5s";
-            graficos[1].style.paddingRight = "55%";
-            graficos[1].style.transition = "1.6s";
-            graficos[2].style.paddingRight = "47%";
-            graficos[2].style.transition = "1.7s";
-            graficos[3].style.paddingRight = "24%";
-            graficos[3].style.transition = "1.8s";
-            graficos[4].style.paddingRight = "22%";
-            graficos[4].style.transition = "1.9s";
-            graficos[5].style.paddingRight = "22%";
-            graficos[5].style.transition = "2s";
-        }
-    } else{
-        if(window.pageYOffset > 300){
-            graficos[0].style.paddingRight = "60%";
-            graficos[0].style.transition = "1.5s";
-            graficos[1].style.paddingRight = "55%";
-            graficos[1].style.transition = "1.6s";
-            graficos[2].style.paddingRight = "47%";
-            graficos[2].style.transition = "1.7s";
-            graficos[3].style.paddingRight = "24%";
-            graficos[3].style.transition = "1.8s";
-            graficos[4].style.paddingRight = "22%";
-            graficos[4].style.transition = "1.9s";
-            graficos[5].style.paddingRight = "22%";
-            graficos[5].style.transition = "2s";
-        }
-    }
+        
+     }  
 }
 
 function trocarProjeto(direcao){
